099faf53380e2105658047202431e4b187932d23
정희 한 <jhhan803@outlook.kr>
2018-11-19 14:49:48 +0900

    PP12.Vector2D

--- GameFrameWork/PP01.HelloSDL/SDLGameObjcet.h
+++ GameFrameWork/PP01.HelloSDL/SDLGameObjcet.h
@@ -1,17 +1,20 @@
 #pragma once
 #include <SDL.h>
 #include "GameObject.h"
+#include "Vector2D.h"
 
 class SDLGameObject : public GameObject
 {
 public:
 	SDLGameObject(const LoaderParams* pParams);
 	virtual void draw();
-	virtual void update() {};
-	virtual void clean() {};
+	virtual void update();
+	virtual void clean();
+
 protected:
-	int m_x;
-	int m_y;
+	Vector2D m_position;
+	Vector2D m_velocity;
+	Vector2D m_acceleration;
 	int m_width;
 	int m_height;
 	int m_currentRow;
